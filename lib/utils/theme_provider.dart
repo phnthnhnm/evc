71eb319a55becedcf452d2d33ed2ecb771ac026f
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-10-14 08:07:35 +0700

    fix(settings): save user's selected theme to shared_preferences

--- lib/utils/theme_provider.dart
+++ lib/utils/theme_provider.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  static const String _themeKey = 'theme';
   ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeProvider() {
+    _loadThemeMode();
+  }
 
   ThemeMode get themeMode => _themeMode;
 
-  void setThemeMode(ThemeMode mode) {
+  void setThemeMode(ThemeMode mode) async {
     _themeMode = mode;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_themeKey, mode.toString().split('.').last);
+  }
+
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeString = prefs.getString(_themeKey);
+    if (themeString != null) {
+      switch (themeString) {
+        case 'light':
+          _themeMode = ThemeMode.light;
+          break;
+        case 'dark':
+          _themeMode = ThemeMode.dark;
+          break;
+        default:
+          _themeMode = ThemeMode.system;
+      }
+      notifyListeners();
+    }
   }
 }

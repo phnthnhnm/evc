fa869647feb7e05ae5ae0f7771d036a791cee2c4
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2026-06-30 16:58:44 +0700

    fix: use context.mounted instead of mounted in echo compare screen

--- lib/features/compare/screens/echo_compare_screen.dart
+++ lib/features/compare/screens/echo_compare_screen.dart
@@ -302,7 +302,7 @@ class _EchoCompareScreenState extends ConsumerState<EchoCompareScreen> {
                                   ).notifier,
                                 )
                                 .applyCompareResult(result);
-                            if (mounted) context.pop();
+                            if (context.mounted) context.pop();
                           },
                         ),
                       ),
